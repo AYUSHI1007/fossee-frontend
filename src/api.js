b6429364fce5765994b796db15491e705c4371e1
AYUSHI <ayushipandeyjamshedpur@gmail.com>
2026-02-04 21:49:14 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const API_BASE = process.env.REACT_APP_API_URL || 'https://fossee-webbasedapp-1.onrender.com/api';
+REACT_APP_API_URL = https://fossee-webbasedapp-1.onrender.com/api;
 
 export function getAuthHeaders() {
   const user = localStorage.getItem('api_user');
